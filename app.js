227cb9c4d2751099faee422b3da679a97cc72b44
chandan7718 <70969125+chandan7718@users.noreply.github.com>
2025-01-25 14:58:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const inputs = document.querySelectorAll(".input-field");
-const toggle_btn = document.querySelectorAll(".toggle");
 const main = document.querySelector("main");
 const bullets = document.querySelectorAll(".bullets span");
 const images = document.querySelectorAll(".image");
@@ -11,12 +10,6 @@ inputs.forEach((inp) => {
   inp.addEventListener("blur", () => {
     if (inp.value != "") return;
     inp.classList.remove("active");
-  });
-});
-
-toggle_btn.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    main.classList.toggle("sign-up-mode");
   });
 });
 
@@ -37,3 +30,13 @@ function moveSlider() {
 bullets.forEach((bullet) => {
   bullet.addEventListener("click", moveSlider);
 });
+
+document.querySelector("form").addEventListener("submit", function (event) {
+        var hcaptchaResponse = document.querySelector(
+          "[name='h-captcha-response']"
+        ).value;
+        if (!hcaptchaResponse) {
+          event.preventDefault();
+          alert("Please complete the hCaptcha.");
+        }
+      });
